8f3b25d62520b1568dd074f150a4ae9bb3fcdd39
Ilya Filyaev <i.filyaev@paysenger.com>
2023-09-11 22:19:50 +0300

    Initial Commit

---
+++ GenerationBright/GenerationBrightApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerationBrightApp.swift
+//  GenerationBright
+//
+//  Created by Илья Филяев on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GenerationBrightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
